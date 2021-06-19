67d2f2c10f3780a248810064cab7be5b6cb508bc
Tyflo29 <tyflo29400@gmail.com>
2021-06-19 19:06:52 +0200

    Ajout

---
+++ Tiled/tilesmap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tilesmap" tilewidth="24" tileheight="24" tilecount="225" columns="15">
+ <image source="tilesmap.png" width="360" height="360"/>
+</tileset>
